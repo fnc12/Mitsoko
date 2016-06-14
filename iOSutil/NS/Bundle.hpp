723e48474f9bd1a7cede6ddde10e3d2ed1b154e0
fnc12 <fnc12@me.com>
2016-06-14 19:53:19 +0600

    added NSBundle to iOSutil

---
+++ iOSutil/NS/Bundle.hpp
@@ -0,0 +1,24 @@
+
+#pragma once
+
+#include "Object.hpp"
+#include "Array.hpp"
+
+namespace NS {
+    struct Bundle:public Object{
+        using Object::Object;
+#ifdef __APPLE__
+        STATIC_VAR(std::string, className, "NSBundle");
+        
+        static Bundle mainBundle(){
+            auto cls=NS::getClass(className());
+            assert(cls);
+            return sendMessage<Handle>(cls, "mainBundle");
+        }
+        
+        NS::Array preferredLocalizations(){
+            return this->sendMessage<Handle>("preferredLocalizations");
+        }
+#endif
+    };
+}
